f269adac07b37cf4e05007147f3378d4b3832783
Aarif Mohd <58023343+aarifmohd1@users.noreply.github.com>
2025-03-07 11:04:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = ${{ secrets.AWS_ACCESS_KEY }}
-  secret_key = ${{ secrets.AWS_SECRET_KEY }}
+  #access_key = ${{ secrets.AWS_ACCESS_KEY }}
+  #secret_key = ${{ secrets.AWS_SECRET_KEY }}
 }
 
 resource "aws_instance" "ec2_example" {
