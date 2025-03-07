339872997393d2be4013928ec7ba5ab731915931
Aarif Mohd <aarmohd1@in.ibm.com>
2025-03-07 11:13:50 +0530

    Removed fmt

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "ec2_example" {
 
-    ami           = "ami-04b4f1a9cf54c11d0"  
-    instance_type = "t2.micro" 
+  ami           = "ami-04b4f1a9cf54c11d0"
+  instance_type = "t2.micro"
 }
